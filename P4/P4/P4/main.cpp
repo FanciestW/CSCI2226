d37553ded13e3223043d871ce0da1a55d066ec7e
William Lin <wlin26@yahoo.com>
2016-03-08 15:46:18 -0500

    Started and worked on program 4

---
+++ P4/P4/P4/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  P4
+//
+//  Created by William Lin on 3/4/16.
+//  Copyright © 2016 William Lin. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
